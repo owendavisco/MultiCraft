6dae13ffa564c6f1ee4c3a4e3e2446e02c75371c
Owen R Davis <owendavisco@gmail.com>
2016-10-13 11:59:53 -0600

    Added basic minecraft proxy

---
+++ bin/MinecraftServer.js
@@ -0,0 +1,8 @@
+class MinecraftServer {
+    constructor(host, port) {
+        this.host = host;
+        this.port = port;
+    }
+}
+
+module.exports = MinecraftServer;
--- bin/ProxyServer.js
+++ bin/ProxyServer.js
@@ -1,16 +1,69 @@
-const net = require('net');
-const EventEmmiter = require('EventEmitter');
+'use strict';
 
-class ProxyServer extends EventEmmiter {
+const net = require('net');
+const EventEmitter = require('events').EventEmitter;
+
+class ProxyServer extends EventEmitter {
     constructor() {
+        super();
         this.clients = {};
         this.servers = {};
         this.socketServer = null;
     }
 
-    listen(port, serverIps) {
+    listen(port, servers) {
+        const self = this;
+        let clientId = 0;
+        let serverId = 0;
 
+        for (let server of servers) {
+            self.servers[serverId] = net.connect(server.port, server.host, function(){
+                console.log("Connected To Server");
+            });
+            serverId++;
+        }
+
+        self.socketServer = net.createServer();
+        self.socketServer.on('connection', socket => {
+            console.log("Connection received");
+            self.clients[clientId] = socket;
+
+            socket.pipe(self.servers[0]);
+            self.servers[0].pipe(socket);
+
+            // socket.on('data', data => {
+            //    for (let i = 0; i < serverId; i++) {
+            //        var server = self.servers[0];
+            //        server.write(data);
+            //    }
+            // });
+
+            self.socketServer.on('error', err => {
+                self.emit('error', err);
+            });
+            self.socketServer.on('close', () => {
+                self.emit('close');
+            });
+            self.socketServer.on('listening', () => {
+                self.emit('listening');
+            });
+        });
+        self.socketServer.listen(port);
     }
 }
 
 module.exports = ProxyServer;
+
+function main() {
+    const server = new ProxyServer();
+    server.listen(8080, [{
+            'port': 25565,
+            'host': 'localhost'
+        },
+        {
+            'port': 25564,
+            'host': 'localhost'
+        }]);
+}
+
+main();
